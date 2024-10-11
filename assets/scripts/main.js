2bf89fb5ed60905ee0508d537b715f66ecb5482f
CaadanGYI <yzekiel.caadan@gmail.com>
2024-10-11 20:18:45 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,28 @@
+/*console.log("helloworld");
+
+let my_Variable = "123";
+
+console.log(my_Variable);
+
+
+
+for(let x=1;x<=100; x++)
+    {
+        if(x%2 ==0)
+        {
+            console.log(x)
+        }
+    
+    }
+
+*/
+
+const body = document.body
+
+const div = document.createElement("div")
+
+body.append(div)
+
+const strong = document.createElement("strong")
+
+strong.innerText = "hello world"
